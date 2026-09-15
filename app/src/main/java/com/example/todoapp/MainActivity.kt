b0d5bb81c88cfdbefa9c6c100c387d4e4da4e21d
siwhitedust <stickgaming27@gmail.com>
2026-09-15 09:11:02 +0700

    menambah detail tampilan

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +8,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -22,20 +23,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,9 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier.padding(innerPadding))
-                }
+                MainScreen(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -55,21 +46,58 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    var listTugas = remember { mutableStateListOf<String>("") }
     var isChecked by remember { mutableStateOf(false) }
-    Scaffold(topBar = {CenterAlignedTopAppBar(title = { Text("Todo App") })}){ innerPadding ->
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("Todo App") })
+        }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding)
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth().padding(20.dp).background(MaterialTheme.colorScheme.surfaceContainerHighest, shape = RoundedCornerShape(16.dp)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
                 verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Checkbox(checked = isChecked, onCheckedChange = { isChecked = it })
-                Text("Belajar Sepeda")
+                Button(onClick = {}) { Text("Tambah") }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {}) { Text("Edit") }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {}) { Text("Hapus") }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .background(
+                        MaterialTheme.colorScheme.surfaceContainerHighest,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TodoItem(isChecked)
             }
         }
+    }
+}
+
+@Composable
+fun TodoItem(cek: Boolean) {
+    var cek by remember { mutableStateOf(cek) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = cek, onCheckedChange = { cek = it })
+        Text("Hello")
     }
 }
